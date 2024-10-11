60a15a231cb4d7da8c5bbaf36477436d35598748
Xuhongze <xhzby1995@163.com>
2024-10-11 16:52:47 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ requirements = [
 
 setup(
     name="qsteed",
-    version="0.1.2",
+    version="0.2.0",
     author="Xuhongze",
     author_email="xhzby1995@163.com",
     url="https://github.com/BAQIS-Quantum/QSteed",
